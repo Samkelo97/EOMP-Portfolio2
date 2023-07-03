e2da1b2f4fe347b9362452b0d4b96df4b5b1ded2
Samkelo <sammpokela@gmail.com>
2023-07-03 12:08:45 +0200

    update

---
+++ src/views/AboutView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>My Story</h1>
+    <div class="myImage">
+      
+
+    </div>
+  </div>
+</template>
+
+<style>
+.about{
+  height: 100vh;
+  background-color: black;
+}
+.myImage{
+ height:300px;
+ width: 300px;
+ background-color: blue;
+  background-repeat:no-repeat;
+  background-size:cover;
+  margin: auto;
+  margin-top: 5%;
+  background-image: url(https://i.postimg.cc/90gq5My6/C12-Samkelo-Mpokela-Class1-croped.jpg);
+  border-radius: 50%;
+
+}
+
+</style>
