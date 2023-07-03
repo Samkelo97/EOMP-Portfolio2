8db1f3e0f2e2e76a9c6ae036e52e81783ba60196
Samkelo <sammpokela@gmail.com>
2023-07-03 11:14:51 +0200

    first

---
+++ src/components/footer.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-body-secondary">© 2023 Company, Inc</p>
+
+    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+    </a>
+
+    <ul class="nav col-md-4 justify-content-center">
+      <li class="nav-item"><router-link to="#" class="nav-link px-2 text-body-secondary">Home</router-link></li>
+      <li class="nav-item"><router-link to="#" class="nav-link px-2 text-body-secondary">About</router-link></li>
+      <li class="nav-item"><router-link to="#" class="nav-link px-2 text-body-secondary">Projects</router-link></li>
+      <li class="nav-item"><router-link to="#" class="nav-link px-2 text-body-secondary">Testimonilas</router-link></li>
+      <li class="nav-item"><router-link to="#" class="nav-link px-2 text-body-secondary">Resume</router-link></li>
+    </ul>
+  </footer>
+</div>
+
+</template>
